19e9661e1329afb889d92e488e48e8bc86ba0cfa
strIDE <133251776+itSpc991@users.noreply.github.com>
2024-07-04 19:42:29 +0800

    vggnet

--- VGG/train.py
+++ VGG/train.py
@@ -1,45 +1,46 @@
 import os
 import sys
 import json
+
 import torch
 import torch.nn as nn
-from torchvision import transforms, datasets, utils
-import matplotlib.pyplot as plt
-import numpy as np
+from torchvision import transforms, datasets
 import torch.optim as optim
 from tqdm import tqdm
-import alexnet
+
+import vggnet
 
 
 def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print("using {} device.".format(device))
-    # 数据预处理
+
     data_transform = {
-        # 随机裁剪,随机翻转,ToTensor,标准化
         "train": transforms.Compose([transforms.RandomResizedCrop(224),
-                                    transforms.RandomHorizontalFlip(),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
+                                      transforms.RandomHorizontalFlip(),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]),
         "val": transforms.Compose([transforms.Resize((224, 224)),
                                     transforms.ToTensor(),
                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])}
 
-    data_root = os.path.abspath(os.path.join(os.getcwd(), "./"))  
-    image_path = os.path.join(data_root, "data", "flower_data")
+    data_root = os.path.abspath(os.path.join(os.getcwd(), "../.."))  # get data root path
+    image_path = os.path.join(data_root, "data_set", "flower_data")  # flower data set path
     assert os.path.exists(image_path), "{} path does not exist.".format(image_path)
     train_dataset = datasets.ImageFolder(root=os.path.join(image_path, "train"),
-                                        transform=data_transform["train"])
+                                          transform=data_transform["train"])
     train_num = len(train_dataset)
 
+    # {'daisy':0, 'dandelion':1, 'roses':2, 'sunflower':3, 'tulips':4}
     flower_list = train_dataset.class_to_idx
     cla_dict = dict((val, key) for key, val in flower_list.items())
+    # write dict into json file
     json_str = json.dumps(cla_dict, indent=4)
-    with open('./AlexNet/class_indices.json', 'w') as json_file:
+    with open('class_indices.json', 'w') as json_file:
         json_file.write(json_str)
 
     batch_size = 32
-    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])
+    nw = min([os.cpu_count(), batch_size if batch_size > 1 else 0, 8])  # number of workers
     print('Using {} dataloader workers every process'.format(nw))
 
     train_loader = torch.utils.data.DataLoader(train_dataset,
@@ -50,33 +51,23 @@ def main():
                                             transform=data_transform["val"])
     val_num = len(validate_dataset)
     validate_loader = torch.utils.data.DataLoader(validate_dataset,
-                                                    batch_size=4, shuffle=False,
-                                                    num_workers=nw)
-
+                                                  batch_size=batch_size, shuffle=False,
+                                                  num_workers=nw)
     print("using {} images for training, {} images for validation.".format(train_num,
-                                                                            val_num))
+                                                                              val_num))
+
     # test_data_iter = iter(validate_loader)
     # test_image, test_label = test_data_iter.next()
-    #
-    # def imshow(img):
-    #     img = img / 2 + 0.5  # unnormalize
-    #     npimg = img.numpy()
-    #     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    #     plt.show()
-    #
-    # print(' '.join('%5s' % cla_dict[test_label[j].item()] for j in range(4)))
-    # imshow(utils.make_grid(test_image))
 
-    net = alexnet.AlexNet(num_classes=5, init_weights=True)
-
+    model_name = "vgg16"
+    net = vggnet.vgg(model_name=model_name, num_classes=5, init_weights=True)
     net.to(device)
     loss_function = nn.CrossEntropyLoss()
-    # pata = list(net.parameters())
-    optimizer = optim.Adam(net.parameters(), lr=0.0002)
+    optimizer = optim.Adam(net.parameters(), lr=0.0001)
 
-    epochs = 10
-    save_path = './AlexNet/AlexNet.pth'
+    epochs = 30
     best_acc = 0.0
+    save_path = './{}Net.pth'.format(model_name)
     train_steps = len(train_loader)
     for epoch in range(epochs):
         # train
@@ -95,12 +86,12 @@ def main():
             running_loss += loss.item()
 
             train_bar.desc = "train epoch[{}/{}] loss:{:.3f}".format(epoch + 1,
-                                                                    epochs,
-                                                                    loss)
+                                                                      epochs,
+                                                                      loss)
 
         # validate
         net.eval()
-        acc = 0.0
+        acc = 0.0  # accumulate accurate number / epoch
         with torch.no_grad():
             val_bar = tqdm(validate_loader, file=sys.stdout)
             for val_data in val_bar:
@@ -111,7 +102,7 @@ def main():
 
         val_accurate = acc / val_num
         print('[epoch %d] train_loss: %.3f  val_accuracy: %.3f' %
-                (epoch + 1, running_loss / train_steps, val_accurate))
+              (epoch + 1, running_loss / train_steps, val_accurate))
 
         if val_accurate > best_acc:
             best_acc = val_accurate
